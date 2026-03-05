2e91ce813d9af58e4eeb38e2b49b7db91343358c
Lemar Tokham <ltokham@gmail.com>
2026-03-05 20:51:19 +0000

    Add login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { coerce, z } from 'zod'
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
 
@@ -120,4 +122,24 @@ export async function deleteInvoices(id: string){
     
     revalidatePath('/dashboard/invoices') // re-fetches new data once the db has been updated so cached data is refreshed and not stale
 
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
